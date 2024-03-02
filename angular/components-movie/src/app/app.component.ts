c39878e66b2d165d78fab9bf8626aeb823cc76e2
ssharma89 <sanusourav056@gmail.com>
2024-03-02 19:51:07 +0530

    update

---
+++ angular/components-movie/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'components-movie';
+  img1= "https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABW0CdL7BuY0VTI81TjA2iMRBryIJt2mUIsehZnbVH_AlD0DAXB5kE9-RJ9f-at0-SPB4PA8QbonFUC3eA-ND3puHO2m-7pab-mCDUA1QTbPbw89q3EkQfjcY9M4NM0rZEnGE.jpg?r=4f5"
+  img2= "https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABVCv4yWZncknTfl2xBuxumpRupouujgaPP6WyW4SKlKW4ca5zcmfhKPeuawwDGILSJ2qg0wpSbTi55egpBHiVpiLFnhxZfcKX7LSA1gC7OM9qpj06RqN3lq8VAnazeKuNq2v.jpg?r=326"
+  img3= "https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABXvQ6mF2upM6koaj9Lo5uNfKEdjrG91EE7vOzDfiOkZ1u2te--0DYs7aWx3aY9jG-rhUOr_1XI4ceZ210A444r2N6GgM_bJlI8gLhKs4uq2tj4Zx7KXKIctnrfH195Gr-LQU.jpg?r=650"
+  img4= "https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABSJjNHN851-Cl06JGyzd6Y7iOQig_ZUrvDsQByiTvwepHgFDZURgWXEuQuec_oJ7F__svBBbNGRBCB6DAOygfXxSkMEZKCkobtxdDFy2TJ_UWrLkq6yMeXjKckt3om5F3_CelASPir0MFbbsro6tb-7Bo42GR6Cz49UrAYBAg2ed1BIkGQm3TEyN7SV7lw4.jpg?r=5da"
+  img5= "https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABfXZ1RjpwsvrwiB42sUFc0MbUsSZ45haoPM5coiR3_hqHVVRJQFSyGmhUjeIHCpctbN0sd1c3TsG1CD-zp-HZHIrBxp2m1NU2Z0Rr_GYp1FvosEyi2xJx-w79vxraWCWNyPO.jpg?r=4fc"
+}
